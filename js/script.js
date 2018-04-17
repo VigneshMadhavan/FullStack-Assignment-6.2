4f1f8e2058fbab6cb0aa7956d5eacc4bda068643
VigneshMadhavan <vigneshmadhavan@yahoo.co.in>
2018-04-17 17:25:37 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var recipeName="Creamy squash linguine";
+var preparationTime=45;
+var recipeBy="Jimmy Cutter";
+var isVegetarian=true;
+var calories=223.7;
+
+console.log('Recipe details');
+console.log('--------------');
+console.log('Recipe Name:' + recipeName);
+console.log('preparation Time:' + preparationTime);
+console.log('Recipe By:' + recipeBy);
+console.log('is Vegetarian Dish:' + isVegetarian);
+console.log('Calories:' + calories);
+
+preparationTime=60;
+recipeBy="Jimmy Cutter George";
+
+
+console.log('Recipe details after update');
+console.log('----------------------------');
+console.log('Recipe Name:' + recipeName);
+console.log('preparation Time:' + preparationTime);
+console.log('Recipe By:' + recipeBy);
+console.log('is Vegetarian Dish:' + isVegetarian);
+console.log('Calories:' + calories);
